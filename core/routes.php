fed0a72bf86cdbf74809d9ffc7ea837c31e46c15
kondou <kondou@ts.unde.re>
2014-08-30 16:03:01 +0200

    Whoops.

--- core/routes.php
+++ core/routes.php
@@ -105,16 +105,6 @@ $this->create('core_share_show_share', '/s/{token}')
 	->get()
 	->action('OC\Core\Share\Controller', 'showShare');
 
-// Not specifically routed
-$this->create('app_index_script', '/apps/{app}/')
-	->defaults(array('file' => 'index.php'))
-	//->requirements(array('file' => '.*.php'))
-	->action('OC', 'loadAppScriptFile');
-$this->create('app_script', '/apps/{app}/{file}')
-	->defaults(array('file' => 'index.php'))
-	->requirements(array('file' => '.*.php'))
-	->action('OC', 'loadAppScriptFile');
-
 // used for heartbeat
 $this->create('heartbeat', '/heartbeat')->action(function(){
 	// do nothing
